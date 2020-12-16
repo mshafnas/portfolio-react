e4214a1d27971fb99eb1784062aa89bf7f45b2e5
Shafnas <mshafnas125@gmail.com>
2020-12-16 21:21:24 +0530

    sidebar on progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+// import components
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div class="row">
+          <div className="col-lg-3 col-md-12 col-sm-12">
+            <Sidebar/>
+          </div>
+          <div className="col-lg-9 col-md-12 col-sm-12 app_main-content">
+            <h5>Side here</h5>
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
